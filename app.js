aeff689e7401ba82de203da16f35d9e64c82ab1b
pratik4891 <pratik.rudra@gmail.com>
2021-06-29 23:43:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const graphqlHTTP = require ('express-graphql').graphqlHTTP;
+const schema = require('./schema/schema');
+
+
+const app = express();
+
+
+app.use('/graphql' , graphqlHTTP (
+
+  {
+  schema,
+  graphiql: true
+
+  }
+)
+
+)
+
+app.listen(4000,() =>
+{
+  console.log('Now listening for requests on port 4000');
+}
+)
